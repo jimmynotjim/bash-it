2c6d970530e30b5251ade5d4e76f63888b58b82a
James Wilson <jimmynotjim@me.com>
2013-11-08 11:04:06 -0500

    Added default aliases for vagrant

---
+++ aliases/available/vagrant.bash
@@ -0,0 +1,11 @@
+cite 'about-alias'
+about-alias 'vagrant'
+
+# Bundler Commands
+alias vup="vagrant up"
+alias vh="vagrant halt"
+alias vs="vagrant suspend"
+alias vr="vagrant resume"
+alias vrld="vagrant reload"
+alias vssh="vagrant ssh"
+alias vstat="vagrant status"
